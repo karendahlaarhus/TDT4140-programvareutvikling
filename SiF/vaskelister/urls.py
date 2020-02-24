e4646533aba11c6747d71baf4c1c08cdef18762e
Tobias Hyldmo <tobiashy@stud.ntnu.no>
2020-02-24 15:25:09 +0100

    fjernet feilmelding på kjøring

--- SiF/vaskelister/urls.py
+++ SiF/vaskelister/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
+from django.views.generic import TemplateView
 
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path("", views.list_lists, name="lists"),
-
+    path('', TemplateView.as_view(template_name='list_lists.html'), name='list_lists'),
 ]
