3609cce94e51e634c793335f5fedef8c25b39c37
fjerdal <fjerdal@stud.ntnu.no>
2020-02-26 14:26:47 +0100

    Added password for bruker

--- SiF/bruker/admin.py
+++ SiF/bruker/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import bruker
 
 class brukerAdmin(admin.ModelAdmin):
-    fields = ['brukernavn', 'isManager', 'kollektiv']
+    fields = ['brukernavn', 'passord','isManager', 'kollektiv']
     list_filter = ('isManager', 'kollektiv')
     search_fields = ('brukernavn',)
 
--- SiF/bruker/models.py
+++ SiF/bruker/models.py
@@ -2,10 +2,10 @@ from django.db import models
 from kollektiv.models import kollektiv
 from studentby.models import studentby
 
-# Create your models here.
 
 class bruker(models.Model):
     brukernavn = models.CharField(max_length=30)
+    passord = models.CharField(max_length=30, default="passord",null=False)
     isManager = models.BooleanField()
     studentby = studentby()
     kollektiv = models.ForeignKey(kollektiv(), null=True, on_delete=models.SET_NULL)
