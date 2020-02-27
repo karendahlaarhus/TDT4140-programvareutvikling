c6b130bb32df794f3187b0f9afb0d0d1902528f1
karendaa <karendaa@stud.ntnu.no>
2020-02-27 10:58:49 +0100

    fixed conflict

--- SiF/SiF/settings.py
+++ SiF/SiF/settings.py
@@ -125,14 +125,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-<<<<<<< HEAD
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_root/')
-LOGIN_REDIRECT_URL = 'home'
-LOGOUT_REDIRECT_URL = 'home'
-=======
+#LOGIN_REDIRECT_URL = 'home'
+#LOGOUT_REDIRECT_URL = 'home'
+
 LOGIN_REDIRECT_URL = 'todo/index'
 LOGOUT_REDIRECT_URL = 'todo/index'
->>>>>>> origin/wip/#15/login_bruker
+
 
