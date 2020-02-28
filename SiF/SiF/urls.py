80018b7a1b3f5fcbaf278f024c02a18195200f48
karendaa <karendaa@stud.ntnu.no>
2020-02-28 14:56:27 +0100

    update

--- SiF/SiF/urls.py
+++ SiF/SiF/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from bruker.views import LoginView
+from django.views.generic.base import TemplateView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
