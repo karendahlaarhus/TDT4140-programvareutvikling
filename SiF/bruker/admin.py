15f488e71b84d08370da5c5cfe7cb0195eedc832
vebjorsv <vebjorsv@stud.ntnu.no>
2020-03-20 16:02:07 +0100

    feil i forrige push

--- SiF/bruker/admin.py
+++ SiF/bruker/admin.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from .models import bruker
 
 class brukerAdmin(admin.ModelAdmin):
-    fields = ['brukernavn', 'passord','isManager', 'kollektiv', 'studentby']
-    list_filter = ('isManager', 'kollektiv', 'studentby')
+    fields = ['brukernavn', 'passord','isManager', 'kollektiv']
+    list_filter = ('isManager', 'kollektiv')
     search_fields = ('brukernavn',)
 
 admin.site.register(bruker, brukerAdmin)
--- SiF/bruker/models.py
+++ SiF/bruker/models.py
@@ -7,9 +7,8 @@ class bruker(models.Model):
     brukernavn = models.CharField(max_length=30, unique=True)
     passord = models.CharField(max_length=30, default="passord",null=False)
     isManager = models.BooleanField()
+    studentby = studentby()
     kollektiv = models.ForeignKey(kollektiv(), null=True, on_delete=models.SET_NULL)
-    studentby = models.ForeignKey(studentby(), null=True, on_delete=models.SET_NULL)
-
     def __str__(self):
         return self.brukernavn
     class Meta:
