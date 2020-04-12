8a5af123bb9671249fb33f4bc489d425b248d0e4
fjerdal <fjerdal@stud.ntnu.no>
2020-04-12 14:30:44 +0200

    Tested studentbyIndex

--- SiF/SiF/settings.py
+++ SiF/SiF/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'c-n*mujx^jbhu8vu5ld6()r!2k$d150s)0tjowehmm7yj)0)$!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'testserver']
 
 
 # Application definition
--- SiF/studentby/tests.py
+++ SiF/studentby/tests.py
@@ -1,3 +1,36 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from bruker.models import bruker
+from studentby.models import studentby
+from kollektiv.models import kollektiv
 
-# Create your tests here.
+
+
+class TestView(TestCase):
+    def test_index_view(self):
+        #setup:
+        studentby.objects.create(navn="Test", adresse="Testeveien")
+        s = studentby.objects.get(navn="Test")
+        kollektiv.objects.create(kollektivNr=1, studentby=s)
+        kollektiv.objects.create(kollektivNr=2, studentby=s)
+        k1 = kollektiv.objects.get(kollektivNr=1, studentby=s)
+        k2 = kollektiv.objects.get(kollektivNr=2, studentby=s)
+        bruker.objects.create(brukernavn="manager1", passord="passord", isManager=True, kollektiv=k1)
+
+        c = Client()
+
+        #tester at studentbyIndex gir riktig studentby og liste over kollektiv
+        url = '/oversikt/'+str(s.id)
+        response = c.get(url)
+        self.assertTemplateUsed(response, 'bruker/managerside.html') #bruker riktig template
+        self.assertEquals(s, response.context['manager_for_by']) #studentbyIndex gir riktig Studentby
+        kollektiv_liste = response.context['kollektiv_list']
+        self.assertTrue((len(kollektiv_liste) == 2) and (k1 in kollektiv_liste) and (k2 in kollektiv_liste)) #studentbyIndex gir riktig kollektiv
+
+
+
+        #response = c.post('/login/', {'Brukernavn': 'manager1', 'Passord': 'passord'}, follow=True)
+        #self.assertTemplateUsed(response, 'bruker/managerside.html')
+        #respons.content
+
+
+
